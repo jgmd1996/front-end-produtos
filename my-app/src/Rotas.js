829d5550b54fa41c1dfcc689814bcc990de60faf
joao gabriel <jgmd1996@gmail>
2022-12-27 20:32:04 -0300

    Criando categoria com validacao e formik unto com yup

--- my-app/src/Rotas.js
+++ my-app/src/Rotas.js
@@ -8,30 +8,18 @@ import AtualizarCategoria from './pages/categoria/AtualizarCategoria'
 //import DeletarCategoria from './pages/categoria/DeletarCategoria'
 //home
 import Home from './pages/Home'
-import Texte from './pages/Texte'
-
-
 
 const Rotas =() => {
   return (
     <div className='routes'>
       <BrowserRouter>
-      {/* <table>
-      
-      <tr><Link to="/">Criar nova categoria</Link><br/></tr><br/>
-      <tr><Link to="/listaCategoria">Lista categoria</Link><br/></tr>
-      <tr><Link to="/criarProduto">Criar novo produto</Link><br/></tr>
-      
-      </table> */}
       <Routes>
 
         <Route path="/" element={<Home />} />
         <Route path="/criarCategoria" element={<CriarCategoria />} />
         <Route path="/listaCategoria" element={<ListaCategoria />} />
         <Route path="/atualizarcategoria" element={<AtualizarCategoria />} />
-        <Route path="/Texte" element={<Texte />} />
-        
-        
+ 
       </Routes>
       </BrowserRouter>
     </div>
--- my-app/src/pages/categoria/CriarCategoria.js
+++ my-app/src/pages/categoria/CriarCategoria.js
@@ -1,18 +1,16 @@
-import { useState, useEffect } from 'react';
-import CampoTexto from '../../componentes/CampoTexto';
 import { Link, useNavigate } from 'react-router-dom';
-import { Form, FormikProvider, useFormik, Formik } from 'formik';//
+import { Form, FormikProvider, useFormik } from 'formik';
 import * as Yup from "yup";
 
 function CriarCategoria() {
-  const [loading, setLoading] = useState(false);
-  
+
+  const navigate = useNavigate();
 
   const RegisterSchema = Yup.object().shape({
     nome: Yup.string()
       .min(2, 'Muito curto!')
       .max(200, 'Muito grande!')
-      .required('Categoria obrigatório!'),
+      .required('Categoria obrigatório!')
   })
 
   const formik = useFormik({//
@@ -20,60 +18,52 @@ function CriarCategoria() {
       nome: ''
     },
     validationSchema: RegisterSchema,
-    onSubmit: (values, actions) => {
-      setLoading(true);
-      setLoading(false);
+
+    onSubmit: async (values) => {
+      const body = { nome: values.nome }
+      const settings = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify(body)
+      };
+      try {
+        const fetchResponse = await fetch('http://localhost:3001/categories', settings);
+        console.log("fetchResponse", fetchResponse);
+        if (fetchResponse.status === 201) {
+          formik.setFieldValue("nome", null);
+          navigate('/listaCategoria', { replace: true });
+          console.log("Chego aqui");
+        }
+      } catch (e) {
+        console.error(e);
+      }
     }
   });
 
-  console.log("formik",formik.values);
-
+  console.log("formik", formik.values);
   const { errors, touched, handleSubmit, getFieldProps } = formik;
-  
-  const salvarCategoria = async (event) => {
-    event.preventDefault();
-    const body = { nome: nome }
-    console.log("Esse aqui e o nome", nome);
-
-    const settings = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
-      body: JSON.stringify(body)
-    };
-    try {
-      const fetchResponse = await fetch('http://localhost:3001/categories', settings);  
-      console.log("fetchResponse",fetchResponse);
-      if (fetchResponse.status === 201) {
-        const data = await fetchResponse.json();
-        setSavedUser(data);
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
   return (
     <>
-    <FormikProvider value={formik}>
-      <Form autoComplete='off' noValidate onSubmit={handleSubmit}>
-      
-                 <div>
-                    <input 
-                    type="text" 
-                    id="nome"
-                    name="nome"
-                    placeholder="Digite seu nome"
-                    {...getFieldProps('nome')}
-                    />
-                    <div>{touched.nome && errors.nome}</div>
-                </div>
-             <button type='submit'  >Criar categoria</button>
-            <Link to="/">home</Link>
-      </Form>
-    </FormikProvider> 
+      <FormikProvider value={formik}>
+        <Form autoComplete='off' noValidate onSubmit={handleSubmit}>
+
+          <div>
+            <input
+              type="text"
+              id="nome"
+              name="nome"
+              placeholder="Digite a categoria"
+              {...getFieldProps('nome')}
+            />
+            <div>{touched.nome && errors.nome}</div>
+          </div>
+          <button type='submit'  >Criar categoria</button>
+          <Link to="/">home</Link>
+        </Form>
+      </FormikProvider>
     </>
   );
 }
